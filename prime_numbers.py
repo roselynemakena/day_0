388e904334416b6120c638eeac66547a617f42fd
makena <roselynemakena@gmail.com>
2017-02-06 17:19:00 +0300

    Refactored code

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,20 +1,14 @@
 def is_number_prime(number):
-	#Number's 0 and 1 are not prime numbers
+	#Number's 0 and 1 are not prime numbers thus you eliminate them
 	if number == 0:
 		return False
-		# print("Is 0")
 	if number == 1:
 		return False
-		# print("Is 1")
 	
 	for x in range(2,number):
 		if number % x == 0:
-			#return False
-			# print("The number is not prime")
 			return False
 	else:
-			#return True
-			# print("Its prime")
 			return True
 
 result = list(filter(is_number_prime, range(10)))
