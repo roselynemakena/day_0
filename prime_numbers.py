352fbe8097b39491be8a6f262fd1ef0c7c988ccc
makena <roselynemakena@gmail.com>
2017-02-06 17:17:46 +0300

    Added filter to prime_number list

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,22 +1,23 @@
 def is_number_prime(number):
 	#Number's 0 and 1 are not prime numbers
 	if number == 0:
-		#return False
-		print("Is 0")
+		return False
+		# print("Is 0")
 	if number == 1:
-		#return False
-		print("Is 1")
+		return False
+		# print("Is 1")
 	
 	for x in range(2,number):
 		if number % x == 0:
 			#return False
-			print("The number is not prime")
+			# print("The number is not prime")
 			return False
-		else:
+	else:
 			#return True
-			print("Its prime")
+			# print("Its prime")
 			return True
 
-is_number_prime(7)
+result = list(filter(is_number_prime, range(10)))
+print(result)
 
 
