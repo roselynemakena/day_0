4ae14145e0a34585ed5e48448420c6683e07ba10
makena <roselynemakena@gmail.com>
2017-02-06 17:09:21 +0300

    Added True and False on for method

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,13 +1,22 @@
 def is_number_prime(number):
 	#Number's 0 and 1 are not prime numbers
+	if number == 0:
+		#return False
+		print("Is 0")
+	if number == 1:
+		#return False
+		print("Is 1")
 	
-	
-	for x in range(number):
+	for x in range(2,number):
 		if number % x == 0:
 			#return False
 			print("The number is not prime")
+			return False
 		else:
 			#return True
-			print("Its not prime")
+			print("Its prime")
+			return True
+
+is_number_prime(7)
 
 
