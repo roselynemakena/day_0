d557a22f5d4f60157554b13cb92661c6cee854bf
makena <roselynemakena@gmail.com>
2017-02-06 18:22:13 +0300

    Modified test folder and added tests

--- prime_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class Prime_Testing(unittest.TestCase):
-	unittest.main()
-	def setup(self):
-		self.assertTrue(False)
-	def raise_error(self):
-		raise RunTimeError('test error')
