ee24291ed3674d4f122adc4acd84d06cfa0bbe08
makena <roselynemakena@gmail.com>
2017-02-06 22:02:32 +0300

    Added negative test and number large test

--- tests/test_prime.py
+++ tests/test_prime.py
@@ -4,15 +4,16 @@ from functions.prime_numbers import is_number_prime
 class Prime_Testing(unittest.TestCase):
 
 	def test_return_error_when_not_int(self):
-		self.assertEqual(is_number_prime("blah"), "input parameter is not an int")
-
+		self.assertEqual(is_number_prime("string"), "input parameter is not an int")
 	def test_is_number_0(self):
-		#should return false
 		self.assertFalse(is_number_prime(0))	
 	def test_is_number_1(self):
-		pass
-	def check_if_number_(self):
-		pass
+		self.assertFalse(is_number_prime(1))	
+	def test_if_number_is_negative(self):
+		self.assertFalse(is_number_prime(-1))
+	def test_if_number_is_too_large(self):
+		self.assertEqual(is_number_prime(10*10),"number is too large")
+		
 
 if __name__ == '__main__':
 	unittest.main()
