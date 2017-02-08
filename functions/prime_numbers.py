95f719747b20c715087ffb006967468f7788f5dd
makena <roselynemakena@gmail.com>
2017-02-08 16:36:26 +0300

    Removed .pwc and .swp files

--- functions/prime_numbers.py
+++ functions/prime_numbers.py
@@ -2,6 +2,8 @@ def is_number_prime(number):
 	#Number's 0 and 1 are not prime numbers thus you eliminate them
 	if not isinstance(number, int):
 		return "input parameter is not an int"
+	if number >= 10*10:
+		return "number is too large"
 	if number < 0:
 		return False
 	if number == 0:
